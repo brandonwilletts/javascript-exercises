ceca27590f4f599514f9b35680c2cc37c1189a6b
Brandon Willetts <brandonwilletts@gmail.com>
2024-04-07 15:11:18 -0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
 
+    if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    } else {
+        let numArray = [num1, num2].sort(function(a,b){return a-b});
+        for (let i = numArray[0]; i <= numArray[1]; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
 };
 
 // Do not edit below this line

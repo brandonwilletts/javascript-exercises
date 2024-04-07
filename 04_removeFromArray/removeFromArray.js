2ca6072c4ab9a42755f4b41fa949a7c22104235c
Brandon Willetts <brandonwilletts@gmail.com>
2024-04-07 10:54:55 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const outputArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (checkArrayItem(array[i], ...args) === false) {
+            outputArray.push(array[i]);          
+        }
+    }
+    return outputArray;
+}
 
-};
-
+function checkArrayItem (arrayItem, ...args) {
+    let result = false;
+    for (let i = 0; i < args.length; i++){
+        if (arrayItem === args[i]){
+            result = true;
+        }
+    }
+    return result;
+}
+    
 // Do not edit below this line
 module.exports = removeFromArray;

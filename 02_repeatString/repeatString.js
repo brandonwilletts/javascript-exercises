208e9e0fb3bbaf1d3d9cf145129e10ef3dc59d75
Brandon Willetts <brandonwilletts@gmail.com>
2024-04-07 08:14:12 -0700

    Complete exercise 02
    Declare output variable, add for loop to generate string, add
    conditional that returns ERROR if input number < 0.p

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let outputString = "";
+    if (times >= 0) {
+        for (i = 0; i < times; i++){
+            outputString += word;
+        }
+        return outputString;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
